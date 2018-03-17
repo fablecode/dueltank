31da5f3c59ce7300b6319d2e5c166c46cb378c1e
fablecode <fablecode@users.noreply.github.com>
2018-03-17 22:08:14 +0000

    JUnit for Jasmine test results

--- src/Angular/dueltank/karma.conf.js
+++ src/Angular/dueltank/karma.conf.js
@@ -11,13 +11,14 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
+      require('karma-junit-reporter'),
       require('@angular/cli/plugins/karma')
     ],
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      reports: [ 'html', 'cobertura'],
+      reports: [ 'html', 'junit'],
       fixWebpackSourcePaths: true
     },
     angularCli: {
