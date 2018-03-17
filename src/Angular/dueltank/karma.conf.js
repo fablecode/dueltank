1bba00aa95a9ecc537623ea86f0f40b8abfe11bc
fablecode <fablecode@users.noreply.github.com>
2018-03-17 22:58:52 +0000

    JUnit for Jasmine test results

--- src/Angular/dueltank/karma.conf.js
+++ src/Angular/dueltank/karma.conf.js
@@ -18,7 +18,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      reports: [ 'html', 'junit'],
+      reports: [ 'html', 'cobertura'],
       fixWebpackSourcePaths: true
     },
     angularCli: {
