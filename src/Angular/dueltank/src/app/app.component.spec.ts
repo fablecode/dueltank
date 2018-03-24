06435b7f939f8b090f86562c628d934178eab444
fablecode <fablecode@users.noreply.github.com>
2018-03-24 08:58:16 +0000

    Fixed broken tests

--- src/Angular/dueltank/src/app/app.component.spec.ts
+++ src/Angular/dueltank/src/app/app.component.spec.ts
@@ -1,11 +1,28 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {SideNavigationDirective} from "./shared/directives/sideNavigation.directive";
+import {SideNavigationComponent} from "./shared/components/sidenavigation/sidenavigation.component";
+import {NavbarComponent} from "./shared/components/navbar/navbar.component";
+import {HomeModule} from "./modules/home/home.module";
+import {BrowserModule} from "@angular/platform-browser";
+import {RouterModule} from "@angular/router";
+import { APP_BASE_HREF } from '@angular/common'
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        NavbarComponent,
+        SideNavigationComponent,
+        SideNavigationDirective
       ],
+      imports: [
+        BrowserModule,
+        HomeModule,
+        RouterModule.forRoot([])
+      ],
+      providers: [{provide: APP_BASE_HREF, useValue : '/' }]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
@@ -18,10 +35,10 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('app');
   }));
-  it('should render title in a h1 tag', async(() => {
+  it('should render wrapper', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    expect(compiled.querySelector('div#page-wrapper')).toBeTruthy();
   }));
 });
