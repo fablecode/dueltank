26cdc0f96f07563f66af73241f3a99f5653e9a41
fablecode <fablecode@users.noreply.github.com>
2018-03-24 23:15:06 +0000

    Forgot password component added.

--- src/Angular/dueltank/src/app/modules/account/account.module.ts
+++ src/Angular/dueltank/src/app/modules/account/account.module.ts
@@ -3,16 +3,19 @@ import {LoginPage} from "./pages/login/login.page";
 import {RouterModule, Routes} from "@angular/router";
 import {SocialLoginComponent} from "./components/socialLogin/socialLogin.component";
 import {RegisterPage} from "./pages/register/register.page";
+import {ForgotPage} from "./pages/password/forgot/forgot.page";
 
 const accountRoutes: Routes = [
   {   path: "login", component: LoginPage},
-  {   path: "register", component: RegisterPage}
+  {   path: "register", component: RegisterPage},
+  {   path: "password/forgot", component: ForgotPage}
 ];
 
 @NgModule({
   declarations: [
     LoginPage,
     RegisterPage,
+    ForgotPage,
     SocialLoginComponent
   ],
   imports: [
---
+++ src/Angular/dueltank/src/app/modules/account/pages/password/forgot/forgot.page.ts
@@ -0,0 +1,7 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: "forgotPassword",
+  templateUrl: "./forgot.page.html"
+})
+export class ForgotPage {}
