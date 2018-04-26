4e22b6585d80d59cadb978efd0a1988b2d36425b
fablecode <fablecode@users.noreply.github.com>
2018-04-26 20:27:58 +0100

    ExternalLogin for social logins added.

--- src/Api/src/dueltank.apis/dueltank.api/Controllers/AccountsController.cs
+++ src/Api/src/dueltank.apis/dueltank.api/Controllers/AccountsController.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using dueltank.api.Models;
+using dueltank.api.Models.AccountViewModels;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace dueltank.api.Controllers
@@ -10,11 +13,101 @@ namespace dueltank.api.Controllers
     [Route("api/[controller]/[action]")]
     public class AccountsController : Controller
     {
-        // GET api/values
-        [HttpGet]
-        public IEnumerable<string> Get()
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountsController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
         {
-            return new string[] { "value1", "value2" };
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        /// <summary>
+        /// Register a new user
+        /// </summary>
+        /// <param name="model"></param>
+        /// <param name="returnUrl"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register(RegisterViewModel model, string returnUrl = null)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+
+                }
+            }
+
+            return BadRequest(ModelState);
+        }
+
+        /// <summary>
+        /// Authenticate an existing user
+        /// </summary>
+        /// <param name="model"></param>
+        /// <param name="returnUrl"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [AllowAnonymous]
+        public Task<IActionResult> Login(LoginViewModel model, string returnUrl = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Logout an authenticated user
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public Task<IActionResult> Logout()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Authenticate using a social login
+        /// </summary>
+        /// <param name="provider"></param>
+        /// <param name="returnUrl"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult ExternalLogin(string provider, string returnUrl = null)
+        {
+            // Request a redirect to the external login provider.
+            var redirectUrl = Url.Action(nameof(ExternalLoginCallback), "Accounts", new { returnUrl });
+            var properties = _signInManager.ConfigureExternalAuthenticationProperties(provider, redirectUrl);
+            return Challenge(properties, provider);
+        }
+
+        /// <summary>
+        /// Social login callback
+        /// </summary>
+        /// <param name="returnUrl"></param>
+        /// <param name="remoteError"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [AllowAnonymous]
+        public Task<IActionResult> ExternalLoginCallback(string returnUrl = null, string remoteError = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Social login confirmation
+        /// </summary>
+        /// <param name="model"></param>
+        /// <param name="returnUrl"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [AllowAnonymous]
+        public Task<IActionResult> ExternalLoginConfirmation(ExternalLoginViewModel model, string returnUrl = null)
+        {
+            throw new NotImplementedException();
         }
     }
 }
