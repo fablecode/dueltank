3136060122350532a036ec7fb4d2bc4b3edd6a38
fablecode <fablecode@users.noreply.github.com>
2018-05-09 08:07:30 +0100

    Register -> Url validation and string validation added to ReturnUrl.

--- src/Api/src/dueltank.api/Models/QueryParameters/RegisterQueryParameters.cs
+++ src/Api/src/dueltank.api/Models/QueryParameters/RegisterQueryParameters.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
 
 namespace dueltank.api.Models.QueryParameters
 {
     public class RegisterQueryParameters
     {
         [BindRequired]
+        [StringLength(2083, MinimumLength = 4)]
+        [Url]
         public string ReturnUrl { get; set; }
     }
 }
