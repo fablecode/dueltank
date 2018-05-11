1c5b77ab6a870eb75fbf1119168701203843b75e
fablecode <fablecode@users.noreply.github.com>
2018-05-11 07:42:22 +0100

    Angular -> Register endpoint added to accounts service.

--- src/Angular/dueltank/src/app/shared/services/accounts.service.ts
+++ src/Angular/dueltank/src/app/shared/services/accounts.service.ts
@@ -3,6 +3,18 @@ import {Observable} from "rxjs/Observable";
 import { AppConfigService} from "./app-config.service";
 import {UserProfile} from "../models/userprofile";
 import {HttpClient} from "@angular/common/http";
+import {catchError} from "rxjs/operators";
+
+export class RegisterUserResponse {
+  token: string;
+  user: UserProfile;
+}
+
+export class RegisterUser {
+  username: string;
+  email: string;
+  password: string;
+}
 
 @Injectable()
 export class AccountsService {
@@ -11,5 +23,9 @@ export class AccountsService {
   public Profile() : Observable<UserProfile> {
     // Make the API call using the new parameters.
     return this.http.get<UserProfile>(this.configuration.apiEndpoint + "/api/accounts/profile");
+  }
+
+  public Register(user : RegisterUser) : Observable<RegisterUserResponse> {
+    return this.http.post<RegisterUserResponse>(this.configuration.apiEndpoint + "/api/accounts/register", user);
   }
 }
