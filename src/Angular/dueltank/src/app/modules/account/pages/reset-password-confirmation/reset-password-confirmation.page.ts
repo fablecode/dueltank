7c6123ace27476dacbe9f8569e6d365610b68fb4
fablecode <fablecode@users.noreply.github.com>
2018-05-24 06:20:15 +0100

    Angular -> globals and isLoggedIn had a private scope. It's now public.

--- src/Angular/dueltank/src/app/modules/account/pages/reset-password-confirmation/reset-password-confirmation.page.ts
+++ src/Angular/dueltank/src/app/modules/account/pages/reset-password-confirmation/reset-password-confirmation.page.ts
@@ -1,8 +1,11 @@
 import {Component} from "@angular/core";
+import {Globals} from "../../../../globals";
 
 @Component({
   templateUrl: "./reset-password-confirmation.page.html"
 })
 export class ResetPasswordConfirmationPage {
 
+  constructor(public globals: Globals) {
+  }
 }
