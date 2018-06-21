9cbc62c2dbc6b7514a80bbd2338bf0675f520acd
fablecode <fablecode@users.noreply.github.com>
2018-06-21 12:27:49 +0100

    junit reporter added.

--- src/Angular/dueltank/karma.conf.js
+++ src/Angular/dueltank/karma.conf.js
@@ -18,7 +18,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, 'coverage'), reports: [ 'html', 'lcovonly', 'cobertura'],
+      dir: require('path').join(__dirname, 'coverage'), reports: [ 'html', 'lcov', 'cobertura'],
       fixWebpackSourcePaths: true
     },
     angularCli: {
@@ -32,7 +32,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_DEBUG,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
     singleRun: true,
     junitReporter: {
       outputDir: '',
