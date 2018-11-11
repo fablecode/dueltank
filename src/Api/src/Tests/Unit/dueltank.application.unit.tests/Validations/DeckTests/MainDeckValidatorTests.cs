5b62db66ba1595286c8245a52d776e12d5116e06
fablecode <fablecode@users.noreply.github.com>
2018-11-11 18:49:30 +0000

    Api: Main deck validator, for validating cards in the main deck, added to solution.

--- src/Api/src/Tests/Unit/dueltank.application.unit.tests/Validations/DeckTests/MainDeckValidatorTests.cs
+++ src/Api/src/Tests/Unit/dueltank.application.unit.tests/Validations/DeckTests/MainDeckValidatorTests.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using AutoFixture;
+﻿using AutoFixture;
 using dueltank.application.Models.Cards.Input;
 using dueltank.application.Models.Decks.Input;
+using dueltank.application.Validations.Decks;
 using FluentAssertions;
-using FluentValidation;
 using FluentValidation.TestHelper;
 using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace dueltank.application.unit.tests.Validations.DeckTests
 {
@@ -152,36 +151,6 @@ namespace dueltank.application.unit.tests.Validations.DeckTests
 
             // Assert
             _sut.ShouldNotHaveValidationErrorFor(md => md.MainDeck, _inputModel);
-        }
-    }
-
-    public class MainDeckValidator : AbstractValidator<DeckInputModel>
-    {
-        private const int MinimumCards = 40;
-        private const int MaximumCards = 60;
-        private static readonly string[] ValidCardTypes = { "monster", "spell", "trap" };
-
-        public MainDeckValidator()
-        {
-            RuleFor(d => d.MainDeck)
-                .Cascade(CascadeMode.StopOnFirstFailure)
-                .NotNull()
-                .NotEmpty()
-                .Must(Between40To60Cards)
-                .WithMessage("{PropertyName} must have at least " + MinimumCards + " to " + MaximumCards + " cards.")
-                .Must(AreAllCardTypesValid)
-                .WithMessage("{PropertyName} has an invalid card.");
-
-        }
-
-        private static bool AreAllCardTypesValid(List<CardInputModel> deck)
-        {
-            return deck.All(c => ValidCardTypes.Contains(c.BaseType, StringComparer.OrdinalIgnoreCase));
-        }
-
-        private static bool Between40To60Cards(List<CardInputModel> deck)
-        {
-            return deck.Count >= MinimumCards && deck.Count <= MaximumCards;
         }
     }
 }
---
+++ src/Api/src/dueltank.application/Validations/Decks/MainDeckValidator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using dueltank.application.Models.Cards.Input;
+using dueltank.application.Models.Decks.Input;
+using FluentValidation;
+
+namespace dueltank.application.Validations.Decks
+{
+    public class MainDeckValidator : AbstractValidator<DeckInputModel>
+    {
+        private const int MinimumCards = 40;
+        private const int MaximumCards = 60;
+        private static readonly string[] ValidCardTypes = { "monster", "spell", "trap" };
+
+        public MainDeckValidator()
+        {
+            RuleFor(d => d.MainDeck)
+                .Cascade(CascadeMode.StopOnFirstFailure)
+                .NotNull()
+                .NotEmpty()
+                .Must(Between40To60Cards)
+                .WithMessage("{PropertyName} must have at least " + MinimumCards + " to " + MaximumCards + " cards.")
+                .Must(AreAllCardTypesValid)
+                .WithMessage("{PropertyName} has an invalid card.");
+
+        }
+
+        private static bool AreAllCardTypesValid(List<CardInputModel> deck)
+        {
+            return deck.All(c => ValidCardTypes.Contains(c.BaseType, StringComparer.OrdinalIgnoreCase));
+        }
+
+        private static bool Between40To60Cards(List<CardInputModel> deck)
+        {
+            return deck.Count >= MinimumCards && deck.Count <= MaximumCards;
+        }
+    }
+}
