b5232e0b47a8da2848e60b98f1b6f74305a475fa
fablecode <fablecode@users.noreply.github.com>
2018-11-12 07:00:53 +0000

    Api: Extra deck validator, which validates extra deck cards, add to solution.

---
+++ src/Api/src/Tests/Unit/dueltank.application.unit.tests/Validations/DeckTests/ExtraDeckValidatorTests.cs
@@ -0,0 +1,88 @@
+﻿using System.Linq;
+using AutoFixture;
+using dueltank.application.Models.Cards.Input;
+using dueltank.application.Models.Decks.Input;
+using dueltank.application.Validations.Decks;
+using FluentAssertions;
+using FluentValidation.TestHelper;
+using NUnit.Framework;
+
+namespace dueltank.application.unit.tests.Validations.DeckTests
+{
+    [TestFixture]
+    public class ExtraDeckValidatorTests
+    {
+        private Fixture _fixture;
+        private DeckInputModel _inputModel;
+        private ExtraDeckValidator _sut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _sut = new ExtraDeckValidator();
+            _inputModel = new DeckInputModel();
+            _fixture = new Fixture();
+        }
+
+        [Test]
+        public void Given_ExtraDeck_When_NumberOfCards_GreaterThan_15Cards_Validation_Fails()
+        {
+            // Arrange
+            const string expected = "Extra deck must be 0 to 15 cards.";
+            _fixture.RepeatCount = 16;
+            _inputModel.ExtraDeck =
+                _fixture
+                    .Build<CardInputModel>()
+                    .With(c => c.BaseType, "synchro")
+                    .Without(c => c.Types)
+                    .CreateMany()
+                    .ToList();
+
+            // Act
+            var results = _sut.ShouldHaveValidationErrorFor(deck => deck.ExtraDeck, _inputModel);
+
+            // Assert
+            results.Should().ContainSingle(err => err.ErrorMessage == expected);
+        }
+
+        [Test]
+        public void Given_ExtraDeck_When_NumberOfCards_Between0And15Cards_Validation_Pass()
+        {
+            // Arrange
+            _fixture.RepeatCount = 10;
+            _inputModel.ExtraDeck =
+                        _fixture
+                            .Build<CardInputModel>()
+                            .With(c => c.BaseType, "fusion")
+                            .Without(c => c.Types)
+                            .CreateMany()
+                            .ToList();
+
+            // Act
+
+            // Assert
+            _sut.ShouldNotHaveValidationErrorFor(deck => deck.ExtraDeck, _inputModel);
+        }
+
+        [Test]
+        public void Given_ExtraDeck_With_15Cards_IfAnyCard_IsInvalidCardType_Validation_Fails()
+        {
+            // Arrange
+            const string expected = "Extra deck has an invalid card.";
+            _fixture.RepeatCount = 15;
+            _inputModel.ExtraDeck =
+                _fixture
+                    .Build<CardInputModel>()
+                    .With(c => c.BaseType, "trap")
+                    .Without(c => c.Types)
+                    .CreateMany()
+                    .ToList();
+
+            // Act
+            var results = _sut.ShouldHaveValidationErrorFor(deck => deck.ExtraDeck, _inputModel);
+
+            // Assert
+            results.Should().ContainSingle(err => err.ErrorMessage == expected);
+        }
+    }
+}
---
+++ src/Api/src/dueltank.application/Validations/Decks/ExtraDeckValidator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using dueltank.application.Models.Cards.Input;
+using dueltank.application.Models.Decks.Input;
+using FluentValidation;
+
+namespace dueltank.application.Validations.Decks
+{
+    public class ExtraDeckValidator : AbstractValidator<DeckInputModel>
+    {
+        private static readonly string[] ValidCardTypes = { "fusion", "xyz", "synchro" };
+
+        public ExtraDeckValidator()
+        {
+            RuleFor(d => d.ExtraDeck)
+                .Cascade(CascadeMode.StopOnFirstFailure)
+                .Must(Between0To15Cards)
+                .WithMessage("{PropertyName} must be 0 to 15 cards.")
+                .Must(AreAllCardTypesValid)
+                .WithMessage("{PropertyName} has an invalid card.");
+        }
+
+        private static bool AreAllCardTypesValid(List<CardInputModel> deck)
+        {
+            return deck.All(c => ValidCardTypes.Contains(c.BaseType, StringComparer.OrdinalIgnoreCase));
+        }
+
+        private static bool Between0To15Cards(List<CardInputModel> deck)
+        {
+            return deck.Count <= 15;
+        }
+    }
+}
