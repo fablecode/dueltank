6a4d49d7ac779a2a0573389834598e5ce94e0ef0
fablecode <fablecode@users.noreply.github.com>
2018-11-28 08:36:07 +0000

    Api: Removed unsed using statements

--- src/Api/src/Tests/Unit/dueltank.application.unit.tests/Validations/DeckTests/YgoProDeckTests/YgoProSideDeckValidatorTests.cs
+++ src/Api/src/Tests/Unit/dueltank.application.unit.tests/Validations/DeckTests/YgoProDeckTests/YgoProSideDeckValidatorTests.cs
@@ -1,10 +1,9 @@
-﻿using System.Linq;
-using AutoFixture;
-using dueltank.application.Validations.Deck;
+﻿using AutoFixture;
 using dueltank.application.Validations.Decks.YgoProDeck;
 using dueltank.core.Models.YgoPro;
 using FluentAssertions;
 using NUnit.Framework;
+using System.Linq;
 
 namespace dueltank.application.unit.tests.Validations.DeckTests.YgoProDeckTests
 {
