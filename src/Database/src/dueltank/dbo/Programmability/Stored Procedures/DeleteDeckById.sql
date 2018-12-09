223e76d49749ef13bb1b18f708c12311aba2bcf0
fablecode <fablecode@users.noreply.github.com>
2018-12-09 16:05:33 +0000

    Database: DeleteDeckById modified to include transaction.

--- src/Database/src/dueltank/dbo/Programmability/Stored Procedures/DeleteDeckById.sql
+++ src/Database/src/dueltank/dbo/Programmability/Stored Procedures/DeleteDeckById.sql
@@ -1,15 +1,17 @@
 ﻿CREATE PROCEDURE [dbo].[DeleteDeckById]
-	@DeckId bigint = NULL,
+	@DeckId bigint,
 	@UserId nvarchar(450)
 AS
 BEGIN
-	SET @DeckId = (SELECT Id FROM dbo.Deck d WHERE d.Id = @DeckId AND d.UserId = @UserId)
+	BEGIN TRANSACTION
+		SET @DeckId = (SELECT Id FROM dbo.Deck d WHERE d.Id = @DeckId AND d.UserId = @UserId)
 
-	IF @DeckId IS NOT NULL
-	BEGIN
-		DELETE FROM dbo.DeckCard WHERE dbo.DeckCard.DeckId = @DeckId
-		DELETE FROM dbo.Deck WHERE dbo.Deck.Id = @DeckId AND dbo.Deck.UserId = @UserId
-	END
+		IF @DeckId IS NOT NULL
+		BEGIN
+			DELETE FROM dbo.DeckCard WHERE dbo.DeckCard.DeckId = @DeckId
+			DELETE FROM dbo.Deck WHERE dbo.Deck.Id = @DeckId AND dbo.Deck.UserId = @UserId
+		END
 
-	SELECT @deckId
+		SELECT @DeckId
+	COMMIT TRANSACTION
 END
