2930993252f44825e7b6fd2b613fd5314cd34f64
fablecode <fablecode@users.noreply.github.com>
2019-01-09 22:54:42 +0000

    Api: Test Codecoverage.runsettings modified to exclude properties.

---
+++ src/Api/src/Tests/Unit/dueltank.api.unit.tests/ControllerTests/DecksContollerTests.cs
@@ -0,0 +1,27 @@
+﻿using dueltank.api.Controllers;
+using dueltank.api.Models;
+using dueltank.tests.core;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace dueltank.api.unit.tests.ControllerTests
+{
+    [TestFixture]
+    [Category(TestType.Unit)]
+    public class DecksContollerTests
+    {
+        private IMediator _mediator;
+        private DecksController _sut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mediator = Substitute.For<IMediator>();
+
+            //_sut = new DecksController(_mediator, new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(),));
+        }
+    }
+}
