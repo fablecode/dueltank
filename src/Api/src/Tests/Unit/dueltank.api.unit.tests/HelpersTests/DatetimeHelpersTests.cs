ad0b64c167bfbc099f9c3d2f17bdd6c739b25050
fablecode <fablecode@users.noreply.github.com>
2019-01-13 14:03:11 +0000

    Api: DateTimeHelpers and UrlHelpers tests added.

---
+++ src/Api/src/Tests/Unit/dueltank.api.unit.tests/HelpersTests/DatetimeHelpersTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using dueltank.api.Helpers;
+using dueltank.tests.core;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace dueltank.api.unit.tests.HelpersTests
+{
+    [TestFixture]
+    [Category(TestType.Unit)]
+    public class DatetimeHelpersTests
+    {
+        [Test]
+        public void Given_A_DateTime_Should_Convert_It_To_Unix_EpochDate()
+        {
+            // Arrange
+            const string expected = "1353369600";
+
+            const string dateString = "2012-11-20T00:00:00Z";
+            var datetime = DateTime.Parse(dateString);
+
+            // Act
+            var result = DatetimeHelpers.ToUnixEpochDate(datetime);
+
+            // Assert
+            result.Should().BeEquivalentTo(expected);
+        }
+    }
+}
---
+++ src/Api/src/Tests/Unit/dueltank.api.unit.tests/HelpersTests/UrlHelpersTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Security.Policy;
+using dueltank.api.Helpers;
+using dueltank.tests.core;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Routing;
+using NUnit.Framework;
+
+namespace dueltank.api.unit.tests.HelpersTests
+{
+    [TestFixture]
+    [Category(TestType.Unit)]
+    public class UrlHelpersTests
+    {
+        [Test]
+        public void Given_A_Url_And_Parameters_Should_Append_Parameters_To_Url()
+        {
+            // Arrange
+            const string expected = "http://www.dueltank.com/register_confirmation?userId=1231321&token=sdfsf0u8w42029u";
+
+            const string url = "http://www.dueltank.com/register_confirmation?userId=1231321";
+
+            var parameters = new NameValueCollection
+            {
+                { "token", "sdfsf0u8w42029u"}
+            };
+
+            // Act
+            var result = UrlHelpers.AppendToReturnUrl(url, parameters);
+
+            // Assert
+            result.Should().BeEquivalentTo(expected);
+        }
+    }
+}
