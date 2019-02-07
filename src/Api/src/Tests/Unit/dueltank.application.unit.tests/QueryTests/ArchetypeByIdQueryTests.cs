987d1c2e23c39241b74a358a1235c4818fdc51c3
fablecode <fablecode@users.noreply.github.com>
2019-02-07 14:15:18 +0000

    ArchetypeById query code coverage.

---
+++ src/Api/src/Tests/Unit/dueltank.application.unit.tests/QueryTests/ArchetypeByIdQueryTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using dueltank.application.Queries.ArchetypeById;
+using dueltank.core.Models.Archetypes;
+using dueltank.core.Models.Cards;
+using dueltank.core.Models.Db;
+using dueltank.core.Services;
+using dueltank.tests.core;
+using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace dueltank.application.unit.tests.QueryTests
+{
+    [TestFixture]
+    [Category(TestType.Unit)]
+    public class ArchetypeByIdQueryTests
+    {
+        private IArchetypeService _archetypeService;
+        private ArchetypeByIdQueryHandler _sut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _archetypeService = Substitute.For<IArchetypeService>();
+
+            _sut = new ArchetypeByIdQueryHandler(_archetypeService);
+        }
+
+        [TestCase(0)]
+        [TestCase(-1)]
+        public async Task Given_An_Invalid_ArchetypeId_Should_Return_Null(long archetypeId)
+        {
+            // Arrange
+            // Act
+            var result = await _sut.Handle(new ArchetypeByIdQuery {ArchetypeId = archetypeId}, CancellationToken.None);
+
+            // Assert
+            result.Should().BeNull();
+        }
+
+        [TestCase(23423)]
+        public async Task Given_An_Valid_ArchetypeId_Should_Return_Archetype(long archetypeId)
+        {
+            // Arrange
+            const int expected = 23423;
+
+            _archetypeService.ArchetypeById(Arg.Any<long>()).Returns(new ArchetypeByIdResult
+            {
+                Archetype = new Archetype
+                {
+                    Id = 23423,
+                    Name = "Blue Eyes Deck",
+                    Updated = DateTime.UtcNow
+                },
+                Cards = new List<CardSearch>()
+            });
+
+            // Act
+            var result = await _sut.Handle(new ArchetypeByIdQuery {ArchetypeId = archetypeId}, CancellationToken.None);
+
+            // Assert
+            result.Id.Should().Be(expected);
+        }
+    }
+}
