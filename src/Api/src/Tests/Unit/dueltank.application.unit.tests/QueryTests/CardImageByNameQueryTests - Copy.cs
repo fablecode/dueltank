2af641080835bd19c7924ac30b4265f135b11601
fablecode <fablecode@users.noreply.github.com>
2019-02-10 21:57:43 +0000

    DeckThumbnailImagePathQuery code coverage.

---
+++ src/Api/src/Tests/Unit/dueltank.application.unit.tests/QueryTests/CardImageByNameQueryTests - Copy.cs
@@ -0,0 +1,97 @@
+﻿using dueltank.application.Queries.DeckThumbnailImagePath;
+using dueltank.core.Services;
+using dueltank.tests.core;
+using FluentAssertions;
+using Microsoft.Extensions.Options;
+using NSubstitute;
+using NUnit.Framework;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace dueltank.application.unit.tests.QueryTests
+{
+    [TestFixture]
+    [Category(TestType.Unit)]
+    public class DeckThumbnailImagePathQueryTests
+    {
+        private IOptions<ApplicationSettings> _settings;
+        private DeckThumbnailImagePathQueryHandler _sut;
+        private IImageService _imageService;
+        private IFileService _fileService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _settings = Substitute.For<IOptions<ApplicationSettings>>();
+            _imageService = Substitute.For<IImageService>();
+            _fileService = Substitute.For<IFileService>();
+
+            _sut = new DeckThumbnailImagePathQueryHandler(_settings, _imageService, _fileService);
+        }
+
+        [TestCase(123131)]
+        public async Task Given_A_DeckId_Should_Return_Image_ContentType(long deckId)
+        {
+            // Arrange
+            const string expected = "image/png";
+
+            _settings.Value.Returns(new ApplicationSettings{ DeckThumbnailImageFolderPath = @"C:Image\Deck\Thumbnail" });
+            _imageService.GetImagePath(Arg.Any<string>(), Arg.Any<string>(), Arg.Any<string>()).Returns(@"C:Image\Deck\Thumbnail\" + deckId + ".png");
+            _fileService.ReadAllBytes(Arg.Any<string>()).Returns(new byte[] { 1, 2, 3 });
+
+            // Act
+            var result = await _sut.Handle(new DeckThumbnailImagePathQuery { DeckId = deckId }, CancellationToken.None);
+
+            // Assert
+            result.ContentType.Should().BeEquivalentTo(expected);
+
+        }
+
+        [TestCase(123131)]
+        public async Task Given_A_DeckId_Should_Return_Image(long deckId)
+        {
+            // Arrange
+            var expected = new byte[] {1, 2, 3};
+
+            _settings.Value.Returns(new ApplicationSettings { DeckThumbnailImageFolderPath = @"C:Image\Deck\Thumbnail" });
+            _imageService.GetImagePath(Arg.Any<string>(), Arg.Any<string>(), Arg.Any<string>()).Returns(@"C:Image\Deck\Thumbnail\" + deckId + ".png");
+            _fileService.ReadAllBytes(Arg.Any<string>()).Returns(new byte[] { 1, 2, 3 });
+
+            // Act
+            var result = await _sut.Handle(new DeckThumbnailImagePathQuery { DeckId = deckId }, CancellationToken.None);
+
+            // Assert
+            result.Image.Should().BeEquivalentTo(expected);
+        }
+
+        [TestCase(123131)]
+        public async Task Given_A_DeckId_Should_Return_Invoke_GetImagePath_Once(long deckId)
+        {
+            // Arrange
+            _settings.Value.Returns(new ApplicationSettings { DeckThumbnailImageFolderPath = @"C:Image\Deck\Thumbnail" });
+            _imageService.GetImagePath(Arg.Any<string>(), Arg.Any<string>(), Arg.Any<string>()).Returns(@"C:Image\Deck\Thumbnail\" + deckId + ".png");
+            _fileService.ReadAllBytes(Arg.Any<string>()).Returns(new byte[] { 1, 2, 3 });
+
+            // Act
+            await _sut.Handle(new DeckThumbnailImagePathQuery { DeckId = deckId }, CancellationToken.None);
+
+            // Assert
+            _imageService.Received(1).GetImagePath(Arg.Any<string>(), Arg.Any<string>(), Arg.Any<string>());
+        }
+
+        [TestCase(123131)]
+        public async Task Given_A_DeckId_Should_Return_Invoke_ReadAllBytes_Once(long deckId)
+        {
+            // Arrange
+            _settings.Value.Returns(new ApplicationSettings { DeckThumbnailImageFolderPath = @"C:Image\Deck\Thumbnail" });
+            _imageService.GetImagePath(Arg.Any<string>(), Arg.Any<string>(), Arg.Any<string>()).Returns(@"C:Image\Deck\Thumbnail\" + deckId + ".png");
+            _fileService.ReadAllBytes(Arg.Any<string>()).Returns(new byte[] { 1, 2, 3 });
+
+            // Act
+            await _sut.Handle(new DeckThumbnailImagePathQuery { DeckId = deckId }, CancellationToken.None);
+
+            // Assert
+            _fileService.Received(1).ReadAllBytes(Arg.Any<string>());
+        }
+    }
+}
--- src/Api/src/dueltank.application/Queries/DeckThumbnailImagePath/DeckThumbnailImagePathQueryHandler.cs
+++ src/Api/src/dueltank.application/Queries/DeckThumbnailImagePath/DeckThumbnailImagePathQueryHandler.cs
@@ -1,29 +1,33 @@
-﻿using System.IO;
-using System.Threading;
-using System.Threading.Tasks;
-using dueltank.application.Helpers;
+﻿using dueltank.core.Services;
 using MediatR;
 using Microsoft.Extensions.Options;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace dueltank.application.Queries.DeckThumbnailImagePath
 {
     public class DeckThumbnailImagePathQueryHandler : IRequestHandler<DeckThumbnailImagePathQuery, DeckThumbnailImagePathQueryResult>
     {
         private readonly IOptions<ApplicationSettings> _settings;
+        private readonly IImageService _imageService;
+        private readonly IFileService _fileService;
 
-        public DeckThumbnailImagePathQueryHandler(IOptions<ApplicationSettings> settings)
+        public DeckThumbnailImagePathQueryHandler(IOptions<ApplicationSettings> settings, IImageService imageService, IFileService fileService)
         {
             _settings = settings;
+            _imageService = imageService;
+            _fileService = fileService;
         }
         public Task<DeckThumbnailImagePathQueryResult> Handle(DeckThumbnailImagePathQuery request, CancellationToken cancellationToken)
         {
             const string defaultImage = "no-deck-thumbnail.png";
 
-            var imageFilePath = ImageHelper.GetImagePath(request.DeckId.ToString(), _settings.Value.DeckThumbnailImageFolderPath, defaultImage);
+            var imageFilePath = _imageService.GetImagePath(request.DeckId.ToString(), _settings.Value.DeckThumbnailImageFolderPath, defaultImage);
 
             var response = new DeckThumbnailImagePathQueryResult
             {
-                Image = File.ReadAllBytes(imageFilePath),
+                Image = _fileService.ReadAllBytes(imageFilePath),
                 ContentType = MimeMapping.MimeUtility.GetMimeMapping(Path.GetExtension(imageFilePath))
             };
 
