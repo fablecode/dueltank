ffb9421ad195ca880db59e02fe63ba78a6270913
fablecode <fablecode@users.noreply.github.com>
2019-02-13 22:46:22 +0000

    MostRecentArchetypesQuery code coverage added.

---
+++ src/Api/src/Tests/Unit/dueltank.application.unit.tests/QueryTests/MostRecentArchetypesQueryTests.cs
@@ -0,0 +1,63 @@
+﻿using dueltank.application.Queries.MostRecentArchetypes;
+using dueltank.core.Models.Archetypes;
+using dueltank.core.Services;
+using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace dueltank.application.unit.tests.QueryTests
+{
+    [TestFixture]
+    public class MostRecentArchetypesQueryTests
+    {
+        private IArchetypeService _archetypeService;
+        private MostRecentArchetypesQueryHandler _sut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _archetypeService = Substitute.For<IArchetypeService>();
+
+            _sut = new MostRecentArchetypesQueryHandler(_archetypeService);
+        }
+
+        [Test]
+        public async Task Given_A_PageSize_Should_Return_Most_Recent_Archetypes()
+        {
+            // Arrange
+            var query = new MostRecentArchetypesQuery();
+
+            _archetypeService.MostRecentArchetypes(Arg.Any<int>()).Returns(new MostRecentArchetypesResult{ Archetypes = new List<ArchetypeSearch>()});
+
+            // Act
+            var result = await _sut.Handle(query, CancellationToken.None);
+
+            // Assert
+            result.Archetypes.Should().BeNull();
+        }
+
+        [Test]
+        public async Task Given_A_PageSize_Should_Invoke_MostRecentArchetypes_Once()
+        {
+            // Arrange
+            var query = new MostRecentArchetypesQuery();
+
+            _archetypeService.MostRecentArchetypes(Arg.Any<int>()).Returns(new MostRecentArchetypesResult
+            {
+                Archetypes = new List<ArchetypeSearch>
+                {
+                    new ArchetypeSearch()
+                }
+            });
+
+            // Act
+            await _sut.Handle(query, CancellationToken.None);
+
+            // Assert
+            await _archetypeService.Received(1).MostRecentArchetypes(Arg.Any<int>());
+        }
+    }
+}
