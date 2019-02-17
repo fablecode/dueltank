c5340c4c574c3f4a638c423f49abba54c550add8
fablecode <fablecode@users.noreply.github.com>
2019-02-17 13:07:54 +0000

    AccountsController -> Logout method code coverage added.

---
+++ src/Api/src/Tests/Unit/dueltank.api.unit.tests/ControllerTests/AccountsControllerTests/LogoutTests.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using dueltank.api.Controllers;
+using dueltank.api.Models;
+using dueltank.application.Configuration;
+using FluentAssertions;
+using MediatR;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace dueltank.api.unit.tests.ControllerTests.AccountsControllerTests
+{
+    public class LogoutTests
+    {
+        private UserManager<ApplicationUser> _userManager;
+        private IMediator _mediator;
+        private AccountsController _sut;
+        private IOptions<JwtSettings> _jwtSettings;
+        private SignInManager<ApplicationUser> _signInManager;
+        private RoleManager<IdentityRole> _roleManager;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _userManager = Substitute.For<UserManager<ApplicationUser>>
+            (
+                Substitute.For<IUserStore<ApplicationUser>>(),
+                Substitute.For<IOptions<IdentityOptions>>(),
+                Substitute.For<IPasswordHasher<ApplicationUser>>(),
+                new IUserValidator<ApplicationUser>[0],
+                new IPasswordValidator<ApplicationUser>[0],
+                Substitute.For<ILookupNormalizer>(),
+                Substitute.For<IdentityErrorDescriber>(),
+                Substitute.For<IServiceProvider>(),
+                Substitute.For<ILogger<UserManager<ApplicationUser>>>()
+            );
+
+            _signInManager = Substitute.For<SignInManager<ApplicationUser>>
+            (
+                _userManager,
+                Substitute.For<IHttpContextAccessor>(),
+                Substitute.For<IUserClaimsPrincipalFactory<ApplicationUser>>(),
+                Substitute.For<IOptions<IdentityOptions>>(),
+                Substitute.For<ILogger<SignInManager<ApplicationUser>>>(),
+                Substitute.For<IAuthenticationSchemeProvider>()
+            );
+
+            _roleManager = Substitute.For<RoleManager<IdentityRole>>
+            (
+                Substitute.For<IRoleStore<IdentityRole>>(),
+                Substitute.For<IEnumerable<IRoleValidator<IdentityRole>>>(),
+                Substitute.For<ILookupNormalizer>(),
+                Substitute.For<IdentityErrorDescriber>(),
+                Substitute.For<ILogger<RoleManager<IdentityRole>>>()
+            );
+
+            _mediator = Substitute.For<IMediator>();
+
+            var logger = Substitute.For<ILogger<AccountsController>>();
+
+            _jwtSettings = Substitute.For<IOptions<JwtSettings>>();
+            _sut = new AccountsController(_userManager, _signInManager, _roleManager, logger, _mediator, _jwtSettings);
+        }
+
+        [Test]
+        public async Task Given_A_User_If_User_Signs_Out_Should_Return_OkResult()
+        {
+            // Arrange
+
+            // Act
+            var result = await _sut.Logout();
+
+            // Assert
+            result.Should().BeOfType<OkResult>();
+        }
+
+        [Test]
+        public async Task Given_A_User_If_User_Signs_Out_Should_Invoke_SignOutAsync_Once()
+        {
+            // Arrange
+
+            // Act
+            await _sut.Logout();
+
+            // Assert
+            await _signInManager.Received(1).SignOutAsync();
+        }
+    }
+}
