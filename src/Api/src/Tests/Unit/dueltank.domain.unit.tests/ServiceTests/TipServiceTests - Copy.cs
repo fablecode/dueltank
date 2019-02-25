47c13117786289515fc25215d1875254eef96edb
fablecode <fablecode@users.noreply.github.com>
2019-02-25 21:55:31 +0000

    UserService code coverage added.

---
+++ src/Api/src/Tests/Unit/dueltank.domain.unit.tests/ServiceTests/TipServiceTests - Copy.cs
@@ -0,0 +1,42 @@
+﻿using dueltank.Domain.Repository;
+using dueltank.Domain.Service;
+using dueltank.tests.core;
+using NSubstitute;
+using NUnit.Framework;
+using System;
+using System.Threading.Tasks;
+
+namespace dueltank.domain.unit.tests.ServiceTests
+{
+    [TestFixture]
+    [Category(TestType.Unit)]
+    public class UserServiceTests
+    {
+        private UserService _sut;
+        private IUserRepository _userRepository;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _userRepository = Substitute.For<IUserRepository>();
+
+            _sut = new UserService(_userRepository);
+        }
+
+        [Test]
+        public async Task Should_Invoke_GetTipsByCardId_Once()
+        {
+            // Arrange
+            var userId = Guid.NewGuid().ToString();
+            const long deckId = 90;
+
+            _userRepository.IsUserDeckOwner(Arg.Any<string>(), Arg.Any<long>()).Returns(true);
+
+            // Act
+            await _sut.IsUserDeckOwner(userId, deckId);
+
+            // Assert
+            await _userRepository.Received(1).IsUserDeckOwner(Arg.Any<string>(), Arg.Any<long>());
+        }
+    }
+}
