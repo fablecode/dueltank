bdc0640d5044bd0b45a5a1aab63352084727b008
fablecode <fablecode@users.noreply.github.com>
2019-02-25 21:21:06 +0000

    RulingService code coverage addede.

---
+++ src/Api/src/Tests/Unit/dueltank.domain.unit.tests/ServiceTests/RulingServiceTests.cs
@@ -0,0 +1,42 @@
+﻿using dueltank.core.Models.Db;
+using dueltank.Domain.Repository;
+using dueltank.Domain.Service;
+using NSubstitute;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using dueltank.tests.core;
+
+namespace dueltank.domain.unit.tests.ServiceTests
+{
+    [TestFixture]
+    [Category(TestType.Unit)]
+    public class RulingServiceTests
+    {
+        private RulingService _sut;
+        private IRulingRepository _rulingRepository;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _rulingRepository = Substitute.For<IRulingRepository>();
+
+            _sut = new RulingService(_rulingRepository);
+        }
+
+        [Test]
+        public async Task Should_Invoke_GetByCardId_Once()
+        {
+            // Arrange
+            const int deckId = 90;
+
+            _rulingRepository.GetByCardId(Arg.Any<long>()).Returns(new List<RulingSection>());
+
+            // Act
+            await _sut.GetRulingsByCardId(deckId);
+
+            // Assert
+            await _rulingRepository.Received(1).GetByCardId(Arg.Any<long>());
+        }
+    }
+}
