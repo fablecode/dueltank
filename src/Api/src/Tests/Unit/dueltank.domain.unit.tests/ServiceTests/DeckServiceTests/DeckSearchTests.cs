baab94f91402797a675c87fe9b329342fe846fbe
fablecode <fablecode@users.noreply.github.com>
2019-02-28 19:38:14 +0000

    DeckSearch code coverage

---
+++ src/Api/src/Tests/Unit/dueltank.domain.unit.tests/ServiceTests/DeckServiceTests/DeckSearchTests.cs
@@ -0,0 +1,52 @@
+﻿using dueltank.core.Models.Search.Decks;
+using dueltank.Domain.Repository;
+using dueltank.Domain.Service;
+using dueltank.tests.core;
+using NSubstitute;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace dueltank.domain.unit.tests.ServiceTests.DeckServiceTests
+{
+    [TestFixture]
+    [Category(TestType.Unit)]
+    public class DeckSearchTests
+    {
+        private DeckService _sut;
+        private IDeckTypeRepository _deckTypeRepository;
+        private ICardRepository _cardRepository;
+        private IDeckRepository _deckRepository;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _deckRepository = Substitute.For<IDeckRepository>();
+            _cardRepository = Substitute.For<ICardRepository>();
+            _deckTypeRepository = Substitute.For<IDeckTypeRepository>();
+            var deckCardRepository = Substitute.For<IDeckCardRepository>();
+
+            _sut = new DeckService
+            (
+                _deckRepository,
+                _cardRepository,
+                _deckTypeRepository,
+                deckCardRepository
+            );
+        }
+
+        [Test]
+        public async Task Given_A_DeckSearchCriteria_Should_Invoke_Search_Once()
+        {
+            // Arrange
+            const int expected = 1;
+
+            _deckRepository.Search(Arg.Any<DeckSearchCriteria>()).Returns(new DeckSearchResult());
+
+            // Act
+            await _sut.Search(new DeckSearchCriteria());
+
+            // Assert
+            await _deckRepository.Received(expected).Search(Arg.Any<DeckSearchCriteria>());
+        }
+    }
+}
