6bdf5a5ae87d4c07fd38b28f784f23fcc5097ccf
fablecode <fablecode@users.noreply.github.com>
2019-03-01 22:32:52 +0000

    DeckTypeService renamed to IDeckTypeService.

--- src/Api/src/dueltank.api/Helpers/UrlHelpers.cs
+++ src/Api/src/dueltank.api/Helpers/UrlHelpers.cs
@@ -17,15 +17,6 @@ namespace dueltank.api.Helpers
                 protocol: scheme);
         }
 
-        public static string ResetPasswordCallbackLink(this IUrlHelper urlHelper, string userId, string code, string scheme, string returnUrl)
-        {
-            return urlHelper.Action(
-                action: nameof(AccountsController.ResetPassword),
-                controller: "Accounts",
-                values: new { userId, code, returnUrl },
-                protocol: scheme);
-        }
-
         public static string AppendToReturnUrl(string returnUrl, NameValueCollection parameters)
         {
             return parameters.AllKeys.Aggregate(returnUrl, (current, key) => QueryHelpers.AddQueryString(current, key, parameters[key]));
