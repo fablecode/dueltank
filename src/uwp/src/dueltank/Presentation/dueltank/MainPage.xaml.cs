2c9f488d38f35dc3f46fba8e67324ac2f0cd6a31
fablecode <fablecode@users.noreply.github.com>
2019-08-20 08:41:10 +0100

    RelayCommand added. Hamburger title command now changes when to reflect current selection navigation  item.

--- src/uwp/src/dueltank/Presentation/dueltank/MainPage.xaml.cs
+++ src/uwp/src/dueltank/Presentation/dueltank/MainPage.xaml.cs
@@ -1,21 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.ApplicationModel.Core;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.Graphics.Display;
 using Windows.UI;
 using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Markup;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
@@ -24,13 +12,15 @@ using Windows.UI.Xaml.Navigation;
 namespace dueltank
 {
     /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
+    ///     An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
             InitializeComponent();
+
+            NavigationCacheMode = NavigationCacheMode.Enabled;
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
@@ -38,22 +28,6 @@ namespace dueltank
             base.OnNavigatedTo(e);
 
             CustomizeTitleBar();
-        }
-
-        void MaximizeWindowOnLoad()
-        {
-            //var view = DisplayInformation.GetForCurrentView();
-
-            //// Get the screen resolution (APIs available from 14393 onward).
-            //var resolution = new Size(view.ScreenWidthInRawPixels, view.ScreenHeightInRawPixels);
-
-            //// Calculate the screen size in effective pixels. 
-            //// Note the height of the Windows Taskbar is ignored here since the app will only be given the maxium available size.
-            //var scale = view.ResolutionScale == ResolutionScale.Invalid ? 1 : view.RawPixelsPerViewPixel;
-            //var bounds = new Size(resolution.Width / scale, resolution.Height / scale);
-
-            //ApplicationView.PreferredLaunchViewSize = new Size(bounds.Width, bounds.Height);
-            //ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
         }
 
         private void CustomizeTitleBar()
@@ -64,28 +38,27 @@ namespace dueltank
 
 
             // customize buttons' colors
-            ApplicationViewTitleBar titleBar = ApplicationView.GetForCurrentView().TitleBar;
+            var titleBar = ApplicationView.GetForCurrentView().TitleBar;
 
             titleBar.BackgroundColor = Colors.Red;
             titleBar.ForegroundColor = Colors.White;
 
 
             titleBar.ButtonBackgroundColor = Colors.Red;
-            titleBar.ButtonHoverBackgroundColor = GetSolidColorBrush("#20FFFFFF ").Color;
+            titleBar.ButtonHoverBackgroundColor = GetSolidColorBrush("#40FFFFFF ").Color;
             titleBar.ButtonHoverForegroundColor = Colors.White;
             titleBar.ButtonInactiveForegroundColor = GetSolidColorBrush("#FFF58989").Color;
             titleBar.ButtonInactiveBackgroundColor = Colors.Red;
-
         }
 
         public SolidColorBrush GetSolidColorBrush(string hex)
         {
             hex = hex.Replace("#", string.Empty);
-            byte a = (byte)(Convert.ToUInt32(hex.Substring(0, 2), 16));
-            byte r = (byte)(Convert.ToUInt32(hex.Substring(2, 2), 16));
-            byte g = (byte)(Convert.ToUInt32(hex.Substring(4, 2), 16));
-            byte b = (byte)(Convert.ToUInt32(hex.Substring(6, 2), 16));
-            SolidColorBrush myBrush = new SolidColorBrush(Windows.UI.Color.FromArgb(a, r, g, b));
+            var a = (byte) Convert.ToUInt32(hex.Substring(0, 2), 16);
+            var r = (byte) Convert.ToUInt32(hex.Substring(2, 2), 16);
+            var g = (byte) Convert.ToUInt32(hex.Substring(4, 2), 16);
+            var b = (byte) Convert.ToUInt32(hex.Substring(6, 2), 16);
+            var myBrush = new SolidColorBrush(Color.FromArgb(a, r, g, b));
             return myBrush;
         }
     }
---
+++ src/uwp/src/dueltank/Presentation/dueltank/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,45 @@
+﻿using dueltank.Helpers;
+using Windows.UI.Xaml.Controls;
+
+namespace dueltank.ViewModels
+{
+
+    public class MainPageViewModel : ViewModel
+    {
+        private RelayCommand<NavigationViewItemInvokedEventArgs > _navigationItemInvoked;
+        private string _hamburgerTitle = "Home";
+
+        public MainPageViewModel()
+        {
+            AppTitle = "DuelTank";
+        }
+
+        public string AppTitle { get; }
+
+        public string HamburgerTitle
+        {
+            get => _hamburgerTitle;
+            set
+            {
+                _hamburgerTitle = value;
+
+                //raise property changed event
+                OnPropertyChanged();
+            }
+        }
+
+        public RelayCommand<NavigationViewItemInvokedEventArgs> NavigationItemInvoked
+        {
+            get
+            {
+                return _navigationItemInvoked ?? (_navigationItemInvoked = new RelayCommand<NavigationViewItemInvokedEventArgs>(OnNavigationItemInvoked, param => true));
+            }
+            set => _navigationItemInvoked = value;
+        }
+
+        private void OnNavigationItemInvoked(NavigationViewItemInvokedEventArgs navigationViewItemInvokedEventArgs)
+        {
+            HamburgerTitle = (string)navigationViewItemInvokedEventArgs.InvokedItem;
+        }
+    }
+}
